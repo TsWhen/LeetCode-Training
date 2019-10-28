b8ab2bbf747aa1deff3e115c279c8c14b6e184ae
TsWhen <jiahaohuangjh@gmail.com>
2019-10-28 17:54:36 +0800

    191028 first commit

---
+++ Dynamic Programming/Medium/300_Longest_Increasing_Subsequence.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        dp = [1] * len(nums)
+        
+        for i in range(1,len(nums)):
+            
+            for j in range(i):
+                
+                if nums[i] > nums[j]:
+                    
+                    dp[i] = max(dp[i],dp[j] + 1 )
+        
+        max_len = 0
+        for i in range(len(dp)):
+            
+            if max_len < dp[i]:
+                
+                max_len = dp[i]
+                
+        return max_len
